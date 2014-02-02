693a8194d8c0c5e1a354c9fdd2844a447642e609
Mohit <mohit.bhakkad@gmail.com>
2014-02-02 17:43:15 +0530

    Transactional Bolts added

---
+++ src/main/java/storm/ubiquitous/bolts/Calculator.java
@@ -0,0 +1,137 @@
+package storm.ubiquitous.bolts;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.json.simple.JSONObject;
+
+import storm.ubiquitous.state.RedisMap;
+import backtype.storm.coordination.BatchOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.FailedException;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseTransactionalBolt;
+import backtype.storm.transactional.ICommitter;
+import backtype.storm.transactional.TransactionAttempt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class Calculator extends BaseTransactionalBolt implements ICommitter, Serializable{
+	
+	private static final long serialVersionUID = -2343991642735232104L;
+
+	@SuppressWarnings("serial")
+	public static class CountValue implements Serializable{
+			CountValue()
+			{
+				count = new HashMap<Object,Integer>();
+			}
+			public HashMap<Object,Integer> prev_count = null;
+	    	public HashMap<Object,Integer> count ;
+		    public BigInteger txid = null;
+		    public long atid = 0L;
+	}
+	
+	public static Map<Object, CountValue> INMEMORYDB = new ConcurrentHashMap<Object, CountValue>();
+	
+	
+	TransactionAttempt _id;
+	BatchOutputCollector _collector;
+	
+	
+	Integer _count = 1;
+	RedisMap mapStore;
+	
+	HashMap<Object, HashMap<Object,Integer>> counter; 
+    HashMap<Object, Integer> h;
+	
+	
+
+	public void prepare(@SuppressWarnings("rawtypes") Map conf, TopologyContext context, 
+			    BatchOutputCollector collector, TransactionAttempt id) 
+	{
+		_id = id;
+		_collector=collector;
+		mapStore=new RedisMap("localhost");
+		counter = new HashMap<Object,HashMap<Object,Integer>>();   
+	}
+   
+	public void execute(Tuple tuple) {
+		
+		JSONObject jsonObj = (JSONObject) tuple.getValue(0);
+        
+        try
+        {
+      	  h = counter.get(jsonObj.get("topic"));
+      	  Integer i = h.get(jsonObj.get("country"));
+            i++;
+            
+            h.put(jsonObj.get("country"),i);
+            counter.put(jsonObj.get("topic"), h);
+            	  
+        }
+        catch(Exception e)
+        {
+      	  h = new HashMap<Object,Integer>();
+      	  h.put(jsonObj.get("country"),1);
+      	  counter.put(jsonObj.get("topic"), h);
+        }
+		
+   }    
+   
+	public void finishBatch() throws FailedException
+	{
+		
+		
+	   	for (Object key : counter.keySet()) 
+	   	{
+	        
+	   		CountValue val = INMEMORYDB.get(key);
+			CountValue newVal;
+	
+		    if (val == null || !val.txid.equals(_id.getTransactionId())) 
+		    {
+	          
+		    	newVal = new CountValue();
+		    	newVal.txid = _id.getTransactionId();
+		    	newVal.atid = _id.getAttemptId();
+		          
+		    	if (val != null) 
+		    	{
+		    		newVal.prev_count = val.count;
+		    		newVal.count = val.count;
+		    	}
+	
+			   	newVal.count = counter.get(key);
+		   	   	INMEMORYDB.put(key, newVal);
+		    }
+	        
+		    else 
+		    {
+		         
+		    	newVal = val;
+		    	System.out.println("Tuple: " +  key + " Txid: " + newVal.txid + " AttemptID: "+ newVal.atid + 
+		       				" is replayed.");
+		    }
+		       
+		    _collector.emit(new Values(_id, key, newVal.count, newVal.prev_count));
+		}
+	       	//Store State
+	   	if(counter.size()>0)
+	   	{
+	    	  	mapStore.setState(_id.getTransactionId().toByteArray(), INMEMORYDB);
+	   	}
+	   	
+   }    
+   
+	public void declareOutputFields(OutputFieldsDeclarer declarer) 
+	{
+     		declarer.declare(new Fields("id","topic", "hash"));
+	}
+}
+ 
---
+++ src/main/java/storm/ubiquitous/bolts/Extractor.java
@@ -0,0 +1,117 @@
+package storm.ubiquitous.bolts;
+
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.mongodb.DBObject;
+
+import backtype.storm.coordination.BatchOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBatchBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+@SuppressWarnings("rawtypes")
+public class Extractor extends BaseBatchBolt 
+{
+
+	private static final long serialVersionUID = 7510735576780935459L;
+	Object _id;
+    BatchOutputCollector _collector;
+    String[] words;
+    JSONArray emitArray = null;
+	  
+
+    public void prepare(Map conf, TopologyContext context, BatchOutputCollector collector, Object id) 
+    {
+    	_collector = collector;
+    	_id = id;
+    }
+    
+    public void execute(Tuple tuple) 
+    {
+    	
+    	try
+        {
+         	  JSONParser parser=new JSONParser();
+              DBObject obj = (DBObject) tuple.getValue(0);
+              
+              String topic = new String();
+              topic = obj.get("topic").toString();
+              
+              
+              obj = (DBObject)obj.get("raw"); 
+              obj = (DBObject)obj.get("statuses");
+              
+              JSONArray array = null;
+			  array = (JSONArray) parser.parse(obj.toString());
+			  JSONObject emitobj = new JSONObject();
+              emitobj.put("topic", topic);
+              
+              
+              
+              String tz = new String();
+              try
+              {
+              
+	              for(int i=0 ; i < array.size();i++)
+	              {
+	              	JSONObject jobj =(JSONObject) array.get(i);
+	              	emitobj.put("tweet",jobj.get("text"));
+	              	
+	              	//timezone logic
+	              	tz = ((JSONObject)jobj.get("user")).get("time_zone").toString();
+	              	if(tz == "None")
+	              		tz = ((JSONObject)jobj.get("user")).get("location").toString();
+	              	if(tz == "None")
+	              		tz = jobj.get("place").toString();
+	              	if(tz == "None")
+	              		tz = "unknown";
+	              		
+	              }
+	          }
+              catch(Exception e)
+              {
+            	  tz = "unknown";
+              }
+              
+              
+              
+             
+               	
+              emitobj.put("country",tz);
+              emitArray.add(emitobj);
+              
+              
+        }
+    	catch(Exception e)
+    	{
+    		System.out.println("UnknownHostException: "+e);
+      		
+    	}
+
+    }
+
+    public void finishBatch() 
+    {
+    	if(emitArray!=null)
+    	{
+	    	for(Object emitObj : emitArray)
+	    	{
+	    		_collector.emit(new Values(_id, emitObj));
+	    	}
+    	}
+    	
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) 
+    {
+    	declarer.declare(new Fields("id", "extracted"));
+    }
+    
+  }
