d23848502e97a34ef126486de4f292222e4f87e4
Abhishek Bhattacharjee <abhishek.bhattacharjee11@gmail.com>
2014-03-01 00:16:57 +0530

    Minor Changes.

--- src/main/java/storm/ubiquitous/bolts/BatchCount.java
+++ src/main/java/storm/ubiquitous/bolts/BatchCount.java
@@ -20,95 +20,100 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 public class BatchCount extends BaseTransactionalBolt implements ICommitter, Serializable{
-	/**
-	 * 
-	 * Committer Bolt
-	 * @author aniket
-	 */
-	private static final long serialVersionUID = -2343991642735232104L;
+    /**
+     * 
+     * Committer Bolt
+     * @author aniket
+     */
+    private static final long serialVersionUID = -2343991642735232104L;
 
-	@SuppressWarnings("serial")
-	public static class CountValue implements Serializable{
-		    public Integer prev_count = null;
-		    public int count = 0;
-		    public BigInteger txid = null;
-		    public long atid = 0L;
-	}
+    @SuppressWarnings("serial")
+    public static class CountValue implements Serializable{
+	public Integer prev_count = null;
+	public int count = 0;
+	public BigInteger txid = null;
+	public long atid = 0L;
+    }
 	
-	public static Map<String, CountValue> INMEMORYDB = new ConcurrentHashMap<String, CountValue>();;
+    public static Map<String, CountValue> INMEMORYDB = new ConcurrentHashMap<String, CountValue>();;
 	
 	
-	TransactionAttempt _id;
-	BatchOutputCollector _collector;
-	String name;
-	Map<String, Integer> counters;
-	Integer _count = 1;
-	RedisMap mapStore;
+    TransactionAttempt _id;
+    BatchOutputCollector _collector;
+    String name;
+    Map<String, Integer> counters;
+    Integer _count = 1;
+    RedisMap mapStore;
+    static int test;
 
-	public void prepare(@SuppressWarnings("rawtypes") Map conf, TopologyContext context, 
-			    BatchOutputCollector collector, TransactionAttempt id) {
-		_id = id;
-		this.counters = new HashMap<String, Integer>();
-		_collector=collector;
-		mapStore=new RedisMap("localhost");
-	}
+    public void prepare(@SuppressWarnings("rawtypes") Map conf, TopologyContext context, 
+			BatchOutputCollector collector, TransactionAttempt id) {
+	_id = id;
+	this.counters = new HashMap<String, Integer>();
+	_collector=collector;
+	mapStore=new RedisMap("localhost");
+    }
    
-	public void execute(Tuple tuple) {
-		name = tuple.getString(1);
-   	
-		if(!counters.containsKey(name)){
-		  counters.put(name, _count);
-		}
+    public void execute(Tuple tuple) {
+	name = tuple.getString(1);
+	test++;
+	if(!counters.containsKey(name)){
+	    counters.put(name, _count);
+	}
 	  
-		else{
-		  _count = counters.get(name) + 1;
-		  counters.put(name, _count);
-		}
-   }    
+	else{
+	    _count = counters.get(name) + 1;
+	    counters.put(name, _count);
+	}
+	//Failing deliberately
+	if(test == 180)
+	    throw new FailedException();
+
+    }    
    
-	public void finishBatch() throws FailedException{
+    public void finishBatch() throws FailedException{
 		
-	   	for (String key : counters.keySet()) {
+	for (String key : counters.keySet()) {
 	        
-		CountValue val = INMEMORYDB.get(key);
-		CountValue newVal;
+	    CountValue val = INMEMORYDB.get(key);
+	    CountValue newVal;
 
-	       	if (val == null || !val.txid.equals(_id.getTransactionId())) {
+	    if (val == null || !val.txid.equals(_id.getTransactionId())) {
 	          
-	    	   	newVal = new CountValue();
-	    	   	newVal.txid = _id.getTransactionId();
-	    	   	newVal.atid = _id.getAttemptId();
+		newVal = new CountValue();
+		newVal.txid = _id.getTransactionId();
+		newVal.atid = _id.getAttemptId();
 	          
-			if (val != null) {
-				newVal.prev_count = val.count;
-				newVal.count = val.count;
-			}
-
-			newVal.count = newVal.count + counters.get(key);
-			INMEMORYDB.put(key, newVal);
-	       	}
-	        
-	       	else {
-	         
-			newVal = val;
-			System.out.println("Tuple: " +  key + " Txid: " + newVal.txid + " AttemptID: "+ newVal.atid + 
-			" is replayed.");
-	       	}
-	        	System.out.println("String: "+key+" NewCount: "+newVal.count+" PrevCount: "
-	        	+newVal.prev_count+" Txid: "+_id.getTransactionId()+" AttemptID: "+ _id.getAttemptId());
-	       
-	       	_collector.emit(new Values(_id, key, newVal.count, newVal.prev_count));
+		if (val != null) {
+		    newVal.prev_count = val.count;
+		    newVal.count = val.count;
 		}
-	       	//Store State
-	   	if(counters.size()>0)
-	      	{
-	    	  	mapStore.setState(_id.getTransactionId().toByteArray(), INMEMORYDB);
-	      	}
+
+		newVal.count = newVal.count + counters.get(key);
+		INMEMORYDB.put(key, newVal);
+	    }
+	        
+	    else {
+	         
+		newVal = val;
+		System.out.println("#Tuple: " +  key + " Txid: " + newVal.txid + " AttemptID: "+ newVal.atid + 
+				   " processing avoided.");
+	    }
+	    System.out.println("String: "+key+" NewCount: "+newVal.count+" PrevCount: "
+			       +newVal.prev_count+" Txid: "+_id.getTransactionId()+" AttemptID: "+ _id.getAttemptId());
+	       
+	    _collector.emit(new Values(_id, key, newVal.count, newVal.prev_count));
+	}
+	//Store State
+	if(counters.size()>0)
+	    {
+		mapStore.setState(_id.getTransactionId().toByteArray(), INMEMORYDB);
+	    }
 	   	
-   }    
+    }    
    
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-     		declarer.declare(new Fields("id","name", "count"));
-   }
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+	declarer.declare(new Fields("id","name", "count"));
+    }
 }
  
